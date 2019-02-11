c85be4055d97948b8bef0807e53020d203ce596c
chris54332 <34180169+chris54332@users.noreply.github.com>
2019-02-11 15:11:51 +0000

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -80,26 +80,14 @@
 
 				
 
-		// Poptrox.
-			$window.on('load', function() {
+		// More button.
+					$banner.find('.more')
+						.addClass('scrolly');
 
-				var $thumbs = $('.thumbnails');
+			}
 
-				if ($thumbs.length > 0)
-					$thumbs.poptrox({
-						onPopupClose: function() { $body.removeClass('is-covered'); },
-						onPopupOpen: function() { $body.addClass('is-covered'); },
-						baseZIndex: 10001,
-						useBodyOverflow: false,
-						overlayColor: '#222226',
-						overlayOpacity: 0.75,
-						popupLoaderText: '',
-						fadeSpeed: 500,
-						usePopupDefaultStyling: false,
-						windowMargin: (skel.breakpoint('small').active ? 5 : 50)
-					});
-
-			});
+		// Scrolly.
+			$('.scrolly').scrolly();
 
 		// Initial scroll.
 			$window.on('load', function() {
